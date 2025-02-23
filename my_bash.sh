ffea780ad08acb58a6dee50ac6fec5d0e0188ed6
Lemi <leminegeso@gmail.com>
2025-02-23 17:12:15 +0300

    Starting to code today, so cool

---
+++ my_bash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir bash
+echo "#!/bin/bash" >alx
+echo "echo \"ALX\"">>alx
+
+echo "#!/bin/bash">school
+echo "School">>school
+
+mkdir js
+touch main.js
+touch index.js
+mkdir c
+touch c_is_fun.c
