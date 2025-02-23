293beb8b4075a52ad1d1d38e9f0e111a4d5a3a27
Lemi <leminegeso@gmail.com>
2025-02-23 17:22:33 +0300

    edit

--- my_bash.sh
+++ my_bash.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-mkdir bash
-echo "#!/bin/bash" >alx
-echo "echo \"ALX\"">>alx
+mkdir 0x03-git/bash
+echo "#!/bin/bash" >0x03-git/bash/alx
+echo "echo \"ALX\"">>0x03-git/bash/alx
 
-echo "#!/bin/bash">school
-echo "School">>school
+echo "#!/bin/bash">0x03-git/bash/school
+echo "School">>0x03-git/bash/school
 
-mkdir js
-touch main.js
-touch index.js
-mkdir c
-touch c_is_fun.c
+mkdir 0x03-git/js
+touch 0x03-git/js/main.js
+touch 0x03-git/js/index.js
+mkdir 0x03-git/c
+touch 0x03-git/c/c_is_fun.c
+
+rmdir c js bash 
+rm alx school main.js index.js c_is_fun.c
